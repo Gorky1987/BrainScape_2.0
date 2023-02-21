1adaeef2c970915d08606e2453b0c5ed441b2fcb
Heinrich Mater <herr.hm@icloud.com>
2023-02-21 15:13:24 +0100

    Initial Commit

---
+++ BrainScape_2.0/BrainScape_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  BrainScape_2_0App.swift
+//  BrainScape_2.0
+//
+//  Created by WorkingAccount on 21.02.23.
+//
+
+import SwiftUI
+
+@main
+struct BrainScape_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
